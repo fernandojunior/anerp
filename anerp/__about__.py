4781f7f27518f8582425252bcb1afeb0df0d5e48
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-18 02:49:48 -0300

    Manually extract the __about__

---
+++ anerp/__about__.py
@@ -0,0 +1,9 @@
+
+__author__ = 'Fernando Felix do Nascimento Junior'
+__email__ = 'fernandojr.ifcg@live.com'
+
+__title__ = "anerp"
+__description__ = "An ERP"
+__url__ = "https://github.com/fernandojunior/anerp/"
+__version__ = '0.0.1'
+__license__ = 'MIT License'
--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 from setuptools import setup, find_packages
 from pip.req import parse_requirements as parse
-import anerp as package
-
-# Gives the relative path of a file from the setup.py
-relpath = lambda filename: os.path.join(os.path.dirname(__file__), filename)
 
 # Parse a requirements file to string list
-requirements = lambda f: [str(i.req) for i in parse(relpath(f), session=False)]
+requirements = lambda f: [str(i.req) for i in parse(f, session=False)]
 
 # Read the README file
 with open("README.rst") as f:
     README = f.read()
 
+# Manually extract the __about__
+__about__ = {}
+with open("anerp/__about__.py") as f:
+    exec(f.read(), __about__)
+
 setup(
-    author=package.__author__,
-    author_email=package.__email__,
+    author=__about__['__author__'],
+    author_email=__about__['__email__'],
     classifiers=[
         'Environment :: Web Environment',
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ' + package.__license__,
+        'License :: OSI Approved :: ' + __about__['__license__'],
         'Natural Language :: English',
         'Operating System :: OS Independent',
         "Programming Language :: Python :: 2",
@@ -33,17 +33,16 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ],  # see more: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    description='An ERP',
+    ],
+    description=__about__['__description__'],
     include_package_data=True,
     install_requires=requirements('requirements.txt'),
-    keywords='anerp',
-    license=package.__license__,
+    license=__about__['__license__'],
     long_description=README,
     name='anerp',
     packages=find_packages(exclude=['tests']),
     platforms='any',
-    url='https://github.com/fernandojunior/anerp',
-    version=package.__version__,
+    url=__about__['__url__'],
+    version=__about__['__version__'],
     zip_safe=False
 )
