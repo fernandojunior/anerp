f750f79d165ac94102d2549d7ae78c63cfe02953
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-18 03:09:16 -0300

    Manually extract the __about__

--- anerp/__about__.py
+++ anerp/__about__.py
@@ -1,3 +1,7 @@
+__all__ = [
+    '__author__', '__email__', '__title__', '__description__', '__url__',
+    '__version__', '__license__'
+]
 
 __author__ = 'Fernando Felix do Nascimento Junior'
 __email__ = 'fernandojr.ifcg@live.com'
