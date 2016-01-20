1e5989d18351633bfda87f9deb2c9b90e5e2b904
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-20 17:36:25 -0300

    Some rest functions to parse request arguments.

--- anerp/restful.py
+++ anerp/restful.py
@@ -3,6 +3,7 @@
 RESTFul module.
 """
 import flask_restful as restful
+from flask_restful.reqparse import Argument, RequestParser
 
 # Alias common Flask-RESTful names
 Resource = restful.Resource
@@ -18,3 +19,26 @@ class Api(restful.Api):
             self.add_resource(cls, *args, **kwargs)
             return cls
         return wrapper
+
+
+def create_parser(select, arguments, sep=' '):
+    parser = RequestParser()
+
+    if isinstance(select, str) and select != '*':
+        select = select.split(sep)
+
+    if isinstance(arguments, dict):
+        for name, values in arguments.items():
+            if select == '*' or name in select:
+                parser.add_argument(name, values)
+        return parser
+
+    for argument in arguments:
+        if select == '*' or argument.name in select:
+            parser.add_argument(argument)
+
+    return parser
+
+
+def parse(select, arguments):
+    return create_parser(select, arguments).parse_args()
