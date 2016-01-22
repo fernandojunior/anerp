76e395b4a1ac6007e92d232160d356c48b7d3f18
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-21 22:37:00 -0300

    Extending flask_restful.reqparse module.

---
+++ anerp/reqparse.py
@@ -0,0 +1,49 @@
+from flask_restful.reqparse import Argument
+from flask_restful.reqparse import RequestParser as OriginalRequestParser
+
+
+class RequestParser(OriginalRequestParser):
+
+    def __init__(self, *args, **kargs):
+        arguments = kargs.pop('arguments', None)
+        select = kargs.pop('select', None)
+        update = kargs.pop('update', None)
+        remove = kargs.pop('remove', None)
+        super(RequestParser, self).__init__(*args, **kargs)
+        if arguments:
+            if select:  # add just selected arguments
+                arguments = dict((k, arguments[k]) for k in select)
+            self.add_arguments(arguments)
+        if update:
+            self.update_arguments(update)
+        if remove:
+            self.remove_arguments(remove)
+
+    def add_arguments(self, arguments):
+        '''Add arguments to be parsed. Accepts either a dictionary of arguments
+        or a list of `Argument`.'''
+        if isinstance(arguments, dict):  # {'argument': {'required': ...}, }
+            arguments = [Argument(*i) for i in arguments.items()]
+        for argument in arguments:
+            self.add_argument(argument)
+
+    def update_arguments(self, dictionary):
+        ''' Update the arguments matching the given dictionary keys. '''
+        for arg in self.args:
+            if arg.name in dictionary.keys():
+                setattr(arg, arg.name, dictionary[arg.name])
+
+    def remove_arguments(self, names):
+        ''' Remove the arguments matching the given names. '''
+        for name in names:
+            self.remove_argument(name)
+
+    def copy(self, update=None, remove=None):
+        ''' Creates a copy with the same set of arguments. '''
+        parsercopy = super(RequestParser, self).copy()
+        # parsercopy = flask_restful.reqparse.RequestParser.copy(self)
+        if update:
+            parsercopy.update_arguments(update)
+        if remove:
+            parsercopy.remove_arguments(remove)
+        return parsercopy
--- anerp/restful.py
+++ anerp/restful.py
@@ -5,7 +5,6 @@ RESTFul module.
 from datetime import datetime
 import flask_restful as restful
 from flask_restful import fields
-from flask_restful.reqparse import Argument, RequestParser
 
 # Alias common Flask-RESTful names
 Resource = restful.Resource
@@ -20,62 +19,6 @@ class Api(restful.Api):
             self.add_resource(cls, *args, **kwargs)
             return cls
         return wrapper
-
-
-def request_parser(*args, **kargs):
-    '''Create or copy a request parser.'''
-    if isinstance(args[0], RequestParser):
-        return copy_request_parser(*args, **kargs)
-    else:
-        return create_request_parser(*args, **kargs)
-
-
-def create_request_parser(arguments, select='*', update=None, remove=None):
-    '''Create a request parser with arguments.'''
-    parser = RequestParser()
-
-    if isinstance(arguments, dict):  # {'argument': {'required': True, ...}, }
-        arguments = [Argument(k, v) for k, v in arguments.items()]
-
-    for argument in arguments:  # Argument list
-        if select == '*' or argument.name in select:
-            parser.add_argument(argument)
-
-    if update:
-        update_request_parser(parser, update)
-
-    if remove:
-        remove_parser_arguments(parser, remove)
-
-    return parser
-
-
-def copy_request_parser(parser, update=None, remove=None):
-    '''Copy a request parser. Arguments can be updated or removed.'''
-    parsercopy = parser.copy()
-    if update:
-        update_request_parser(parsercopy, update)
-    if remove:
-        remove_parser_arguments(parsercopy, remove)
-    return parsercopy
-
-
-def update_request_parser(parser, arguments):
-    '''Update request parser arguments.'''
-    for arg in parser.args:
-        if arg.name in arguments.keys():
-            setattr(arg, arg.name, arguments[arg.name])
-
-
-def remove_parser_arguments(parse, arguments):
-    '''Remove request parser arguments.'''
-    for argument in arguments:
-        parse.remove_argument(argument)
-
-
-def parse_request(arguments, select='*'):
-    '''Parse selected request arguments based on a list of arguments.'''
-    return create_request_parser(arguments, select).parse_args()
 
 
 type_mapper = {
