e66c5b13f946fd4c94b36c36bd37658c3f2f7723
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-21 22:37:28 -0300

    Updating rest API for users.

--- anerp/views/user.py
+++ anerp/views/user.py
@@ -2,7 +2,8 @@
 """User views."""
 from flask import Blueprint, render_template
 from flask_login import login_required
-from anerp.restful import Api, marshal_with, request_parser, Resource
+from anerp.restful import Api, marshal_with, Resource
+from anerp.reqparse import RequestParser
 from anerp.models.user import User
 
 blueprint = Blueprint('user', __name__, static_folder='static')
@@ -24,11 +25,11 @@ arguments = {
     'first_name': {'help': 'The user\'s first name'},
     'last_name': {'help': 'The user\'s last name'}}
 
-patch_parser = request_parser(arguments, remove=['id'])  # create new parser
 
-post_parser = request_parser(  # copy patch_parser
-    patch_parser,
-    update={  # update some arguments
+patch_parser = RequestParser(arguments=arguments, remove=['id'])
+
+post_parser = patch_parser.copy(
+    update={
         'username': {'required': True},
         'email': {'required': True},
         'password': {'required': True}},
@@ -74,5 +75,6 @@ class UserList(Resource):
     @marshal_with(public_fields)
     def post(self):
         args = post_parser.parse_args()
+        print(args)
         user = User.create(**args)
         return user
