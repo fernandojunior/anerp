2a34b1a9ec3b964aad3c0bc83c600c39f53c153e
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-25 13:48:55 -0300

    Modul. api.

--- anerp/restful.py
+++ anerp/restful.py
@@ -3,8 +3,7 @@
 RESTFul module.
 """
 from datetime import datetime
-from flask import jsonify as original_jsonify
-from flask_restful import fields, marshal
+from flask_restful import fields
 
 
 type_mapper = {
@@ -19,3 +18,56 @@ type_mapper = {
 def create_marshaller(keys, types):
     '''Create fields based on default Python types to use with marshal'''
     return {k: type_mapper[v] for k, v in zip(keys, types)}
+
+
+from flask import Blueprint
+from anerp.utils import jsonify
+from anerp.reqparse import RequestParsers
+
+
+class Api:
+
+    def __init__(self, model, request_parsers, marshaller):
+        self.model = model
+        self.request_parsers = RequestParsers(request_parsers)
+        self.marshaller = marshaller
+        self.app = Blueprint(model.__name__, __name__, static_folder='static')
+        self.url_rules()
+
+    def init_api(self, app):
+        app.register_blueprint(self.app)
+
+    def add_url_rule(self, rule, view_func):
+        name = view_func.__name__
+        self.app.add_url_rule(rule, name, view_func, methods=[name.upper()])
+
+    def url_rules(self):
+        self.add_url_rule('/', self.get)
+        self.add_url_rule('/<int:id>', self.get)
+        self.add_url_rule('/<int:id>', self.patch)
+        self.add_url_rule('/<int:id>', self.delete)
+        self.add_url_rule('/', self.post)
+
+    @property
+    def query(self):
+        return self.model.query
+
+    def get(self, id=None):
+        data = self.query.get_or_404(id) if id else self.query.all()
+        return jsonify(data, self.marshaller)
+
+    def patch(self, id):
+        obj = self.query.get_or_404(id)
+        args = self.request_parsers('patch')
+        for key, value in args.items():
+            setattr(obj, key, value)
+        obj.update()
+        return self.get(id)
+
+    def delete(self, id):
+        self.query.get_or_404(id).delete()
+        return '', 204
+
+    def post(self):
+        args = self.request_parsers('post')
+        return self.get(self.model.create(**args).id)
--- anerp/views/user.py
+++ anerp/views/user.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 '''User views.'''
-from flask import Blueprint
 from flask_restful import fields
-from anerp.reqparse import RequestParser
-from anerp.models.user import User
-from anerp.restful import jsonify
 from anerp.utils import Dictionary
+from anerp.restful import Api
+from anerp.models.user import User
 
-Model = User
-
-blueprint = Blueprint(Model.__name__, __name__, static_folder='static')
 
 request_arguments = Dictionary({
     'username': {'help': 'The user\'s username'},
@@ -30,7 +25,7 @@ request_parsers = {
     )
 }
 
-marshaller_fields = Dictionary({
+marshaller = {
     'id': fields.Integer,
     'username': fields.String,
     'email': fields.String,
@@ -39,37 +34,7 @@ marshaller_fields = Dictionary({
     'full_name': fields.String,
     'active': fields.Boolean,
     'create_at': fields.DateTime(dt_format='iso8601')
-})
-
-marshallers = {
-    'get': marshaller_fields()
 }
 
-
-@blueprint.route('/')
-@blueprint.route('/<int:id>')
-def get(id=None):
-    data = Model.query.get_or_404(id) if id else Model.query.all()
-    return jsonify(data, marshallers['get'])
-
-
-@blueprint.route('/<int:id>', methods=['PATCH'])
-def patch(id):
-    obj = Model.query.get_or_404(id)
-    args = RequestParser(arguments=request_parsers['patch']).parse()
-    for key, value in args.items():
-        setattr(obj, key, value)
-    obj.update()
-    return get(id)
-
-
-@blueprint.route('/<int:id>', methods=['DELETE'])
-def delete(id):
-    Model.query.get_or_404(id).delete()
-    return '', 204
-
-
-@blueprint.route('/', methods=['POST'])
-def post():
-    args = RequestParser(arguments=request_parsers['post']).parse()
-    return get(Model.create(**args).id)
+api = Api(User, request_parsers, marshaller)
+blueprint = api.app
